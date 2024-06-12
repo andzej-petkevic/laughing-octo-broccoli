c914560bf1d79cf63f802118923b266e7ce31d3b
andzej-petkevic <andzej.petkevic@gmail.com>
2024-06-12 22:54:38 +0300

    init broccoli

---
+++ app/broccoli/src/main/java/dev/andzej/broccoli/BroccoliApplication.java
@@ -0,0 +1,13 @@
+package dev.andzej.broccoli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BroccoliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BroccoliApplication.class, args);
+    }
+
+}
